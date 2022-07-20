4fbf11183f323cf38ca8448a7f8cdd3f067d41a2
marcelinaciupak <marcelina.ciupak@gmail.com>
2022-07-20 11:07:05 +0200

    Add IoCExtesion and register in program.cs

---
+++ FieldBooking/IoCExtenstion.cs
@@ -0,0 +1,26 @@
+﻿using FieldBooking.Data.Repository;
+using FieldBooking.Domain.Repository;
+using FieldBooking.Services;
+
+namespace FieldBooking
+{
+    public static class IoCExtenstion
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBookingService, BookingService>();
+        }
+
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IFieldRepository, FieldRepository>();
+        }
+
+        public static void RegisterValidators(this IServiceCollection services)
+        {
+
+        }
+    }
+}
