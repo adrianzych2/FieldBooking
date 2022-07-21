7603ca09ced781d146af6c944b6753af35d95fa7
marcelinaciupak <marcelina.ciupak@gmail.com>
2022-07-21 14:32:21 +0200

    Create Models: CreateApplicationUser, LoginApplicationUser, Controller: AccountController

--- Domain/Models/CreateApplicationUserDto.cs
+++ Domain/Models/CreateApplicationUserDto.cs
@@ -7,7 +7,5 @@
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Phone { get; set; }
-        public string UserRoles { get; set; }
     }
 }
