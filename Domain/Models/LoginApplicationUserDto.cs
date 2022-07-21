04b137cac9c7a209560d6b7f4e39f59b73953904
marcelinaciupak <marcelina.ciupak@gmail.com>
2022-07-21 14:32:21 +0200

    Create Models: CreateApplicationUser, LoginApplicationUser, Controller: AccountController

---
+++ Domain/Models/LoginApplicationUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FieldBooking.Domain.Models
+{
+    public class LoginApplicationUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FieldBooking/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using FieldBooking.Data.Models;
+using FieldBooking.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FieldBooking.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        public readonly UserManager<ApplicationUser> _userManager;
+        public readonly SignInManager<ApplicationUser> _signInManager;
+        public readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost("registration")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] CreateApplicationUserDto newUser)
+        {
+            var applicationUser = _mapper.Map<ApplicationUser>(newUser);
+            var result = await _userManager.CreateAsync(applicationUser, newUser.Password);
+            if(result.Succeeded)
+            {
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(applicationUser);
+
+                _userManager.ConfirmEmailAsync(applicationUser, token);
+
+                return Ok(result);
+            }
+            return NotFound(result);
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginApplicationUserDto loginUser)
+        {
+            var applicationUser = _mapper.Map<ApplicationUser>(loginUser);
+            var foundUser = await _userManager.FindByEmailAsync(loginUser.Email);
+
+            if (foundUser == null)
+            {
+                return NotFound();
+            }
+            var result = await _signInManager.PasswordSignInAsync(foundUser, loginUser.Password, true, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCurrent()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return Ok(user);
+        }
+    }
+}
